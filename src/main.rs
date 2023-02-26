eaaa3d2874e530a56a9188bb860ec455328503cf
mwu515 <mian.wu515@duke.edu>
2023-02-26 15:54:50 +0000

    an actix solution done. successfully launch localhost

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,13 @@
 /* 
 Routes:
 A. GET /
-B. GET /possible-genres
-C. GET /<genre>
-D. GET /<other stuff>
+B. GET /<genre>
 */
 
+use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
 use base64::{engine::general_purpose, Engine as _};
 use serde::{Deserialize, Serialize};
+
 
 #[derive(Debug, Deserialize, Serialize)]
 struct AccessTokenResponse {
@@ -19,27 +19,30 @@ struct AccessTokenResponse {
     expires_in: u32,
 }
 
-fn get_access_token(
+async fn get_access_token(
     client_id: &str,
     client_secret: &str,
 ) -> Result<String, Box<dyn std::error::Error>> {
-    let client = reqwest::blocking::Client::new();
+    let client = reqwest::Client::new();
     let body = "grant_type=client_credentials";
     let basic_auth = general_purpose::STANDARD.encode(format!("{}:{}", client_id, client_secret));
 
     let response = client
-        .post("https://accounts.spotify.com/api/token")
-        .header(
-            reqwest::header::AUTHORIZATION,
-            format!("Basic {}", basic_auth),
-        )
-        .header(
-            reqwest::header::CONTENT_TYPE,
-            "application/x-www-form-urlencoded",
-        )
-        .body(body)
-        .send()?
-        .json::<AccessTokenResponse>()?;
+    .post("https://accounts.spotify.com/api/token")
+    .header(
+        reqwest::header::AUTHORIZATION,
+        format!("Basic {}", basic_auth),
+    )
+    .header(
+        reqwest::header::CONTENT_TYPE,
+        "application/x-www-form-urlencoded",
+    )
+    .body(body)
+    .send()
+    .await?
+    .json::<AccessTokenResponse>()
+    .await?;
+
     Ok(response.access_token)
 }
 
@@ -59,11 +62,11 @@ struct Artist {
     name: String,
 }
 
-fn get_recommendations(
+async fn get_recommendations(
     access_token: &str,
     genre: &str,
-) -> Result<Vec<Track>, Box<dyn std::error::Error>> {
-    let client = reqwest::blocking::Client::new();
+) -> Result<Vec<Track>, reqwest::Error> {
+    let client = reqwest::Client::new();
     let mut headers = reqwest::header::HeaderMap::new();
     headers.insert(
         reqwest::header::AUTHORIZATION,
@@ -74,20 +77,72 @@ fn get_recommendations(
         .get("https://api.spotify.com/v1/recommendations")
         .headers(headers)
         .query(&[("seed_genres", genre)])
-        .send()?
-        .json::<RecommendationResponse>()?;
+        .send()
+        .await?
+        .json::<RecommendationResponse>()
+        .await?;
 
     Ok(response.tracks)
 }
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let access_token = get_access_token("369dbd7452cf4e5fac093191ef5e6538", "12125765ddf14ff086d8b3252969f7a6")?;
-    // println!("Access token: {}", access_token);
-    
+#[get("/")]
+async fn index() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
+}
+
+// write a method to request recommendations based on a genre
+// return a list of recommendations formatted as a string
+// e.g. "Track 1 by Artist 1, Track 2 by Artist 2, ..."
+async fn get_recommendations_based_on_genre(access_token_data: String) -> Result<String, reqwest::Error> {
+    let access_token = &access_token_data;
     // print out the recommendations for the genre "dance"
-    let recommendations = get_recommendations(&access_token, "dance")?;
+    // TODO: make this a parameter
+    let recommendations = get_recommendations(access_token, "dance").await?;
+    // return a list of recommendations formatted as a string
+    // e.g. "Track 1 by Artist 1, Track 2 by Artist 2, ..."
+    let mut recommendations_string = String::new();
     for track in recommendations {
-        println!("{} by {}", track.name, track.artists[0].name);
+        let mut line = format!("{} by {}, ", track.name, track.artists[0].name);
+        line += "\n";
+        recommendations_string.push_str(&line);
+        // recommendations_string.push_str(&format!("{} by {}, ", track.name, track.artists[0].name));
     }
-    Ok(())
+    Ok(recommendations_string)
+}
+
+#[get("/{genre}")]
+async fn recommend(access_token_data: web::Data<String>, genre: web::Path<String>) -> impl Responder {
+    match get_recommendations_based_on_genre(access_token_data.get_ref().clone()).await {
+        Ok(recommendations) => {
+            println!("Recommendations: {}", recommendations);
+            // Return an HTTP response
+            HttpResponse::Ok().body(format!("Recommendations for {} genre \n\n{}", genre, recommendations))
+        }
+        Err(e) => {
+            eprintln!("Error getting recommendations: {}", e);
+            HttpResponse::InternalServerError().body("Error getting recommendations")
+        }
+    }}
+
+#[get("/{other}")]
+async fn other() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let access_token = get_access_token("369dbd7452cf4e5fac093191ef5e6538", "12125765ddf14ff086d8b3252969f7a6").await.unwrap();
+    let access_token_data = web::Data::new(access_token.clone());
+     HttpServer::new(move || {
+        App::new()
+            // Register the access_token_data as a global state for the application
+            .app_data(access_token_data.clone())
+            .service(index)
+            //.service(possible_genres)
+            .service(recommend)
+            .service(other)
+    })
+    .bind("0.0.0.0:8080")?
+    .run()
+    .await
 }
